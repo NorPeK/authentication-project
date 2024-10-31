7359600205131506d299f84600f6afdbe1294846
NorPeK <norpekbusiness@hotmail.com>
2024-10-31 11:07:55 +0300

    Signup page UI completed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import Input from '../components/input';
 import { User , Mail, Lock} from "lucide-react";
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PasswordStrengthMeter from '../components/PasswordStrengthMeter'
 
 const SignUpPage = () => {
 
@@ -52,7 +53,9 @@ const SignUpPage = () => {
                     onChange={(e) => setPassword(e.target.value)}
                 />
 
-                {/* Password Strength meter */}
+                <PasswordStrengthMeter password={password}/>
+
+
                 <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600
                  text-white font-bold rounded-lg shadow-lg hover:from-green-600
                  hover:to-emerald-700 focus:outline-none focus:ring-2
